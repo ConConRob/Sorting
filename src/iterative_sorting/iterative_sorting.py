df1ffd67ce8ea2d67cb7a5e60eb633b9ec94e494
conconrob <connortprobertson@gmail.com>
2019-06-03 13:40:53 +0100

    done iterative sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,4 +1,7 @@
 # TO-DO: Complete the selection_sort() function below
+import random
+
+
 def selection_sort(arr):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
@@ -37,5 +40,36 @@ def bubble_sort(arr):
 
 # STRETCH: implement the Count Sort function below
 def count_sort(arr, maximum=-1):
+    # find max if max not given
+    if maximum == -1:
+        for num in arr:
+            if num > maximum:
+                maximum = num
+    # make array of zeros up to max the zero index is counts of zero so + 1
+    count = [0 for i in range(maximum + 1)]
+    # count each number
+    for num in arr:
+        if num < 0:
+            return "Error, negative numbers not allowed in Count Sort"
+        count[num] += 1
 
+    for i in range(1, maximum + 1):
+        # add previous count to current
+        count[i] += count[i - 1]
+
+    # shift the array right
+    for i in reversed(range(1, maximum + 1)):
+        count[i] = count[i - 1]
+    # set index zero to be zero
+    if len(arr) > 0:
+        count[0] = 0
+    # count now has the starting indexs so map them
+    for i in range(maximum + 1):
+        # cover end case
+        if i == maximum:
+            for j in range(count[i], len(arr)):
+                arr[j] = i
+        else:
+            for j in range(count[i], count[i+1]):
+                arr[j] = i
     return arr
